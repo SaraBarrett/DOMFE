dde292ddcd115033233d3bc47483c748fd7e9a99
SaraBarrett <sara.c.mon@gmail.com>
2024-10-16 15:37:20 +0100

    events

---
+++ js/classlist.js
@@ -0,0 +1,28 @@
+function changeHighlight(){
+    let myLis = document.getElementsByTagName("li");
+
+    for (element of myLis) {
+        if (element.classList.contains("highlight")) {
+            element.classList.remove("highlight");
+        } else {
+            element.classList.add("highlight");
+        }
+    }
+}
+
+function diminuir(){
+    return 'olá';
+}
+
+function aumentar(){
+    return 'olá';
+}
+
+function hello(){
+    //criar o paragrafo
+    //cria os botões
+
+    //chama a função diminuir no elemento botão que criei, no atributo onclick
+    //chama a função diminuir no elemento botão que criei, no atributo onclick
+    
+}
--- js/index.js
+++ js/index.js
@@ -2,6 +2,9 @@
 // console.log(myBanner);
 
 // let myH1 = document.querySelector("h1");
+// myH1.classList.add("mySuperClass");
+
+// console.log(myH1.classList.contains("mySuperClass"));
 // console.log(myH1);
 
 //selecionar um elemento para manipular o seu texto
@@ -9,11 +12,37 @@
 // myH2.innerHTML = "<b>História</b>";
 // myH2.style.backgroundColor = "green";
 
-
-
 // let myImage = document.querySelector("#banner");
 // myImage.src = "../images/lucas-calloch-P-yzuyWFEIk-unsplash.jpg";
- let myPs = document.querySelectorAll('p');
- for(element of myPs){
-    element.style.backgroundColor = 'blue';
- }
+// let myPs = document.querySelectorAll("p");
+// for (element of myPs) {
+//   element.style.backgroundColor = "blue";
+// }
+
+// let myImage = document.createElement("img");
+// myImage.src = "../images/lucas-calloch-P-yzuyWFEIk-unsplash.jpg";
+
+// let myProduct = document.createElement("div");
+// myProduct.innerText = "produto para maiores de 18";
+
+// document.body.appendChild(myImage);
+
+function hello() {
+  alert("olá turma, está tempo de estar em casa!");
+}
+function hello2() {
+  alert("Estamos quase em React");
+}
+
+function tellMeWhat() {
+  console.log(this);
+}
+
+let myButton = document.querySelector("#unique-id");
+
+// myButton.addEventListener("click", hello, { once: true });
+// myButton.addEventListener("click", hello2);
+
+let myImg = document.getElementById("banner");
+myImg.addEventListener('click', tellMeWhat);
+myButton.addEventListener("click", tellMeWhat);
